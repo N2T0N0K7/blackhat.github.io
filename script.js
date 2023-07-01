2cb80aa4421a8021377a1db5c92cbde1da1cb3e1
SpeedKenode <speedkenode@gmail.com>
2023-07-01 07:57:02 +0700

    fix

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";  
     setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
+
+function searchMenu() {
+  // Declare variables
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("mySearch");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("sidebar");
+  li = ul.getElementsByTagName("a");
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
